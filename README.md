18c41a5cac84c7dcb1f1655bbed81dd0589df628
Logan Swartzendruber <logan.swartzendruber@gmail.com>
2021-08-21 14:22:22 -0400

    Add mock transfers for testing

--- README.md
+++ README.md
@@ -9,6 +9,26 @@ project so that the web interface didn't need to constantly poll the backend.
 ## Usage
 ```bash
 ~ $ git clone https://github.com/loganswartz/rclone-ws-connector && cd rclone-ws-connector
+~/rclone-ws-connector $ npm install
 ~/rclone-ws-connector $ RCLONE_URL="http://localhost:5572" npm start
 ```
-To build a production version, run `npm build`.
+To build a production version, run `npm run build`, and then run the compiled
+version with `node dist/app.js`.
+
+## Installation as a service
+Link the service file into your systemd service folder (typically
+`/etc/systemd/system/`) by running `sudo ln -s
+/path/to/rclone-ws-connector/rclone-ws-connector.service /etc/systemd/system/`.
+Then, create an override for the service file like so:
+```bash
+~ $ sudo mkdir /etc/systemd/system/rclone-ws-connector.service.d
+~ $ sudo nvim /etc/systemd/system/rclone-ws-connector.service.d/override.conf
+```
+(if you don't know how to use vim / neovim, substitute `nvim` for `nano`.)
+Then, paste the following into that file, and save it:
+```
+[Service]
+Environment="RCLONE_URL=<URL or IP + Port of your Rclone API>"
+```
+Reload the systemctl daemon with `sudo systemctl daemon-reload` and then start
+the service with `sudo systemctl start rclone-ws-connector`.
--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,10 @@ import WebSocket from 'ws';
 import http from 'http';
 import url from 'url';
 import util from 'util';
+import { performance } from 'perf_hooks';
 
 
+const testing = false;
 const port = process.env.PORT || 5573;
 const rcloneURL = (process.env.RCLONE_URL || 'http://localhost:5572') + '/core/stats';
 let pollInterval = 1;
@@ -16,6 +18,167 @@ const server = http.createServer(app);
 const wss = new WebSocket.Server({ noServer: true });
 let cached = null;
 
+
+const testTransfers = [
+  {
+    name: "[HorribleSubs]Shingeki.no.Kyojin.Season.4.The.Final.Season.E4.Pain.(1080p).mkv",
+    size: 937912872,
+  },
+  {
+    name: "[Golumpa]Your.Name.2018.(1080p).mkv",
+    size: 14532678327,
+  },
+];
+
+type Transfer = {
+  bytes: number,
+  speed: number,
+  eta: number,
+  name: string,
+  percentage: number,
+  speedAvg: number,
+  size: number,
+};
+
+export type RawStatsReport = {
+  speed: number,
+  bytes: number,
+  errors: number,
+  fatalError: boolean,
+  retryError: boolean,
+  checks: number,
+  transfers: number,
+  deletes: number,
+  renames: number,
+  transferTime: number,
+  elapsedTime: number,
+  lastError?: string,
+  transferring: Transfer[],
+  checking?: string[],
+}
+
+type MockTransfer = {
+  name: string,
+  size: number,
+  minSpeed?: number,
+  maxSpeed?: number,
+  maxAcceleration?: number
+}
+
+function randInt(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+function constrain(value: number, min: number, max: number) {
+  if (value < min) {
+    return min;
+  } else if (value > max) {
+    return max;
+  } else {
+    return value;
+  }
+}
+
+function mockTransfer(props: MockTransfer) {
+  const {
+    name,
+    size,
+    minSpeed = 100,
+    maxSpeed = 50000000,
+    maxAcceleration = 1000000,
+  } = props;
+
+  function* mock() {
+    let progress = 0;
+    let transferRate = 1000000;
+    let acceleration = 0;
+    let timeElapsed = 0;
+    let speedHistory = [];
+    let lastCallTime = performance.now();
+    let lastTransfer: Transfer;
+
+    while (progress < size) {
+      // calculate time since last call
+      let now = performance.now();
+      timeElapsed = (now - lastCallTime) / 1000;
+      lastCallTime = now;
+      speedHistory.push([transferRate, timeElapsed]);
+
+      // if the transfer rate is below 60% of the max, it'll only go up
+      let top = 1;
+      let bottom = 0.6;
+      const range_eccentricity = (value: number, bottom: number, top: number) => {
+        // eccentricity of a value in the context of bottom and top
+        // eccentricity < 0 means value < bottom, eccentricity > 1 means value > top
+        return (value - bottom) * (top / (top - bottom));
+      }
+
+      let eccentricity = constrain(range_eccentricity(transferRate/maxSpeed, bottom, top), 0.000001, 1);
+      let rand = Math.random() / eccentricity;
+      let directionality = Math.round(rand) ? 1 : -1;
+      acceleration = randInt(0, maxSpeed / eccentricity) * directionality;
+      acceleration = constrain(acceleration, -maxAcceleration, maxAcceleration);
+      transferRate = constrain(transferRate + (acceleration * timeElapsed), minSpeed, maxSpeed);
+      // console.log(`${name[1]}: ${transferRate}`);
+      // console.log(`R: ${transferRate}`);
+      // console.log(`E: ${eccentricity}`);
+      // console.log(`D: ${directionality}`);
+      // console.log(`A: ${acceleration}`);
+
+      // calculate total transferred in interval
+      let transferred = Math.round(transferRate * timeElapsed);
+      progress = progress + transferred;
+
+      const transfer: Transfer = {
+        bytes: constrain(progress, 0, size),
+        speed: transferRate,
+        eta: constrain(Math.round((size - progress) / transferRate), 0, Infinity),
+        name: name,
+        percentage: constrain(Math.round((progress / size) * 100), 0, 100),
+        speedAvg: Math.round(
+          speedHistory.reduce((acc, [speed, time]) => acc + (speed * time), 0)
+          / speedHistory.reduce(((acc, [_, time]) => acc + time), 0)
+        ) || 0,
+        size: size,
+      }
+      lastTransfer = transfer;
+      yield transfer;
+    }
+    while (true) {
+      yield Object.assign({}, lastTransfer, { bytes: size, percentage: 100, speed: 0, eta: 0 });
+    }
+    return lastTransfer;
+  }
+  return mock();
+}
+
+const mockGenerators = testTransfers.map(mockTransfer);
+const mockAPI = AsyncPolling((end: Function) => {
+  const transferring: Transfer[] = mockGenerators.map((generator) => generator.next().value);
+  const report: RawStatsReport = {
+    speed: transferring.reduce((acc, item) => acc + item.speed, 0) / transferring.length,
+    bytes: transferring.reduce((acc, item) => acc + item.size, 0),
+    errors: 0,
+    fatalError: false,
+    retryError: false,
+    checks: 0,
+    transfers: transferring.length,
+    deletes: 0,
+    renames: 0,
+    transferTime: 0,
+    elapsedTime: 0,
+    transferring: transferring,
+  }
+  console.log("Got updated info.");
+  wss.clients.forEach((socket: WebSocket) => {
+    if (socket.readyState === WebSocket.OPEN) {
+      socket.send(JSON.stringify(report));
+    }
+  });
+  cached = report;
+  end();
+}, pollInterval * 1000);
+
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
@@ -24,27 +187,30 @@ function heartbeat() {
   this.isAlive = true;
 }
 
-wss.on('connection', (ws, req) => {
+wss.on('connection', (ws: WebSocket, req: Request) => {
   console.log('> Client connected.');
 
   ws.isAlive = true;
   ws.on('pong', heartbeat);
-  ws.on('message', (msg) => {
+  ws.on('message', (msg: string) => {
     ws.send(msg);
   });
 
-  ws.send(JSON.stringify(cached));
+  if (cached !== null) {
+    ws.send(JSON.stringify(cached));
+  }
 });
 
-wss.on('close', (ws) => {
+wss.on('close', (ws: WebSocket) => {
   console.log('< Client Disconnected.');
 });
 
 server.on('upgrade', (request, socket, head) => {
-  const pathname = url.parse(request.url).pathname;
+  const baseURL = `http://${request.headers.host}/`;
+  const pathname = new url.URL(request.url, baseURL).pathname;
 
   if (pathname === '/stats') {
-    wss.handleUpgrade(request, socket, head, (ws) => {
+    wss.handleUpgrade(request, socket, head, (ws: WebSocket) => {
       wss.emit('connection', ws, request);
     });
   } else {
@@ -52,13 +218,13 @@ server.on('upgrade', (request, socket, head) => {
   }
 });
 
-const rcloneAPI = AsyncPolling(end => {
-  fetch(rcloneURL, { method: 'POST' }).then(res => res.json()).then(data => {
+const rcloneAPI = AsyncPolling((end: Function) => {
+  fetch(rcloneURL, { method: 'POST' }).then((res: Response) => res.json()).then((data: object) => {
     const cloned = Object.assign({}, data, { elapsedTime: undefined });
     const previous = Object.assign({}, cached, { elapsedTime: undefined });
     if (!util.isDeepStrictEqual(cloned, previous)) {
       console.log("Got updated info.");
-      wss.clients.forEach(socket => {
+      wss.clients.forEach((socket: WebSocket) => {
         if (socket.readyState === WebSocket.OPEN) {
           socket.send(JSON.stringify(data));
         }
@@ -81,7 +247,7 @@ class Heartbeat {
 
   start() {
     this.interval = setInterval(function ping() {
-      wss.clients.forEach(function each(ws) {
+      wss.clients.forEach(function each(ws: WebSocket) {
         if (ws.isAlive === false) {
           console.log('Heartbeat lost');
           return ws.terminate();
@@ -99,7 +265,11 @@ class Heartbeat {
 }
 
 server.listen(port, () => {
-  rcloneAPI.run();
+  if (testing) {
+    mockAPI.run();
+  } else {
+    rcloneAPI.run();
+  }
   const heartbeat = new Heartbeat(wss, 30);
   heartbeat.start();
 
